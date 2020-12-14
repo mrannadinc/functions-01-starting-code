5108cf41812768827b91ad223ffba043b649d901
Halil İbrahim <h.ibrahim2321@gmail.com>
2020-12-14 14:05:19 +0300

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+
+const getPlayerChoice = function() {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();  
+  if (
+    selection !== ROCK &&
+    selection !== PAPER &&
+    selection !== SCISSORS
+  ) {
+    alert('Invalid choice! We chose Rock for you!');
+  }
+}
+
+startGameBtn.addEventListener('click', function() {
+  console.log('Game starting...');
+});
